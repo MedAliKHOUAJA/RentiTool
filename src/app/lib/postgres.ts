f8102563cd044c513a951d93ad51938bf8d42e30
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2025-10-20 01:35:19 +0100

    Fix: Correct database environment variable name

--- src/app/lib/postgres.ts
+++ src/app/lib/postgres.ts
@@ -5,7 +5,7 @@ let pool: Pool;
 
 if (process.env.NODE_ENV === 'production') {
   pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
