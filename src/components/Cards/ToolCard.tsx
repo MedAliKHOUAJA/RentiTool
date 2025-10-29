2bbd715de2a169840777297bced34aa517bf8410
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2025-10-29 20:45:38 +0100

    fixer build errors + update reviews to respect mobile first rules

--- src/components/Cards/ToolCard.tsx
+++ src/components/Cards/ToolCard.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
-import { ToolDataType } from "@/data/types";
 import StartRating from "@/components/StartRating";
 import BtnLikeIcon from "@/components/BtnLikeIcon";
 import SaleOffBadge from "@/components/SaleOffBadge";
 import Badge from "@/shared/Badge";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
+import { ToolDataType } from "@/features/tools/presentation/tool.dto";
 
 export interface ToolCardProps {
   className?: string;
